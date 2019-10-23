92a85c1170498ea05d827bbf68132c511846caf8
unknown <jinyoung777@gmail.com>
2019-10-23 13:22:33 +0900

    router for root and about page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // async componentDidMount() {
@@ -27,16 +31,30 @@ class App extends Component {
   }
 
   clearUsers = () => this.setState({users: [], loading: false});
-
+ 
+  setAlert = (msg, type) => {
+    this.setState({alert: { msg, type }});
+    setTimeout(() => this.setState({alert: null}), 5000);
+  }
   render() {
     return (
-      <div className="App">
-        <NavBar title="Github Finder"/>
-        <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0}/>
-          <Users loading={this.state.loading} users={this.state.users}/>
+      <Router>
+        <div className="App">
+          <NavBar title="Github Finder"/>
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0} setAlert={this.setAlert}/>
+                  <Users loading={this.state.loading} users={this.state.users}/>
+                </Fragment>
+              )}/>
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
