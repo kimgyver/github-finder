a95742071cd7df9b350f808472910a7fd00ebfd2
unknown <jinyoung777@gmail.com>
2019-10-24 21:06:26 +0900

    converted state into context, added home & notfound components, prepared deployment

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,13 +1,18 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({user, getUser, repos, getUserRepos, match, loading}) => {
+const User = ({match}) => {
+    const githubContext = useContext(GithubContext);
+
+    const {user, getUser, loading, repos, getUserRepos} = githubContext;
+
     useEffect(() => {
         getUser(match.params.login);
         getUserRepos(match.params.login);
+        // eslint-disable-next-line
     }, []);
 
     const {
@@ -82,13 +87,5 @@ const User = ({user, getUser, repos, getUserRepos, match, loading}) => {
         </Fragment>
     )
 }
-
-User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-};
 
 export default User;
