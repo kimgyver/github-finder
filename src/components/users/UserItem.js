c8ccab54c52169b58e34b8e62c0e67f0b59cc9f8
unknown <jinyoung777@gmail.com>
2019-10-25 11:42:04 +0900

    small change

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const UserItem = ({ user: { login, avatar_url, html_url }}) => {
+const UserItem = ({ user: { login, avatar_url, html_url, score }}) => {
     // const { login, avatar_url, html_url } = props.user;
    return (
         <div className="card text-center">
             <img src={avatar_url} alt='' className='round-img' style={{ width: '60px'}} />
             <h3>{login}</h3>
-
+            <h4>score: {parseFloat(score).toFixed(2)}</h4>
             <div>
                 <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
                     More
