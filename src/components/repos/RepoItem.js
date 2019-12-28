03b474d6f3d6c234ae170787622bf1bc3d501064
unknown <jinyoung777@gmail.com>
2019-12-28 17:08:19 +1300

    added repo info

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const RepoItem = ({repo}) => {
-    return (
-        <div className='card'>
-            <h3>
-                <a href={repo.html_url}>{repo.name}</a>
-            </h3>
-        </div>
-    );
+const RepoItem = ({ repo }) => {
+  return (
+    <div className='card'>
+      <div>
+        <h3>
+          <a href={repo.html_url}>{repo.name}</a>
+        </h3>
+        <div className='badge badge-light'>Updated</div>
+        {repo.updated_at
+          ? repo.updated_at
+              .split('T')[0]
+              .split('-')
+              .reverse()
+              .join('/')
+          : ''}
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <div className='badge badge-light'>Language</div> {repo.language}
+      </div>
+      <div>
+        <div className='badge badge-light'>Description</div>
+        {repo.description}
+      </div>
+    </div>
+  );
 };
 
 RepoItem.propTypes = {
-    repo: PropTypes.object.isRequired,
+  repo: PropTypes.object.isRequired
 };
 
 export default RepoItem;
